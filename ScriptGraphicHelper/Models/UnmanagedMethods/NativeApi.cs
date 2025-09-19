ec26099157548f039eeceeadf24cf3fb6720e43f
xxxue <1659809758@qq.com>
2025-09-19 23:33:35 +0800

    refactor: 方向键移动鼠标改为c#调用系统接口
    原c++实现不知道为啥会报错闪退

--- ScriptGraphicHelper/Models/UnmanagedMethods/NativeApi.cs
+++ ScriptGraphicHelper/Models/UnmanagedMethods/NativeApi.cs
@@ -1,24 +1,81 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace ScriptGraphicHelper.Models.UnmanagedMethods
 {
 
     public static class NativeApi
     {
+        //[DllImport("./Assets/mouse")]
+        //public static extern void Move2Left();
+        //[DllImport("./Assets/mouse")]
+        //public static extern void Move2Top();
+        //[DllImport("./Assets/mouse")]
+        //public static extern void Move2Right();
+        //[DllImport("./Assets/mouse")]
+        //public static extern void Move2Bottom();
 
-        [DllImport("./Assets/mouse")]
-        public static extern void Move2Left();
+        [DllImport("user32.dll")]
+        public static extern bool SetCursorPos(int X, int Y);
 
-        [DllImport("./Assets/mouse")]
-        public static extern void Move2Top();
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(ref Point lpPoint);
 
-        [DllImport("./Assets/mouse")]
-        public static extern void Move2Right();
+        /// <summary>
+        /// 鼠标左移 1 像素
+        /// </summary>
+        public static void Move2Left()
+        {
+            Point currentPos = new Point();
+            GetCursorPos(ref currentPos);
 
-        [DllImport("./Assets/mouse")]
-        public static extern void Move2Bottom();
+            int newX = currentPos.X - 1;
+            int newY = currentPos.Y;
 
+            SetCursorPos(newX, newY);
+        }
 
+        /// <summary>
+        /// 鼠标上移 1 像素
+        /// </summary>
+        public static void Move2Top()
+        {
+            Point currentPos = new Point();
+            GetCursorPos(ref currentPos);
+
+            int newX = currentPos.X;
+            int newY = currentPos.Y - 1;
+
+            SetCursorPos(newX, newY);
+        }
+
+        /// <summary>
+        /// 鼠标右移 1 像素
+        /// </summary>
+        public static void Move2Right()
+        {
+            Point currentPos = new Point();
+            GetCursorPos(ref currentPos);
+
+            int newX = currentPos.X + 1;
+            int newY = currentPos.Y;
+
+            SetCursorPos(newX, newY);
+        }
+
+        /// <summary>
+        /// 鼠标下移 1 像素 
+        /// </summary>
+        public static void Move2Bottom()
+        {
+            Point currentPos = new Point();
+            GetCursorPos(ref currentPos);
+
+            int newX = currentPos.X;
+            int newY = currentPos.Y + 1;
+
+            SetCursorPos(newX, newY);
+        }
 
         //由于avalonia的fileDialog在win上会偶发ui阻塞问题, 原因不明, 暂时用win32api替代
         [DllImport("Comdlg32.dll", CharSet = CharSet.Auto)]
